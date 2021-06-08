91687057d4d3324e97775cf856a78cd8cf35f865
Apoorv Maheshwari <63242652+Apoorv-cloud@users.noreply.github.com>
2021-06-08 21:02:47 +0530

    This solves #1

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,17 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    systemNavigationBarColor: Colors.white, // navigation bar color
-    statusBarColor: Colors.white, // status bar color
-    statusBarIconBrightness: Brightness.dark, // status bar icon color
-    systemNavigationBarIconBrightness: Brightness.dark, // color of navigation controls
-  ));
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.white, // navigation bar color
+      statusBarColor: Colors.white, // status bar color
+      statusBarIconBrightness: Brightness.dark, // status bar icon color
+      systemNavigationBarIconBrightness:
+          Brightness.dark, // color of navigation controls
+    ),
+  );
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -24,12 +26,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TEKSNA',
       debugShowCheckedModeBanner: false,
-
       home: MyHomePage(),
     );
   }
 }
-
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -43,55 +43,60 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Image.asset(
-          "assets/teksna.jpeg",
-          height: 80.0,
-          width: 120.0,
-        ),
-          actions: <Widget>[
-               Container(
-                 padding: EdgeInsets.only(right:8, top:15, bottom:15),
-                child:Image.asset("assets/mall-bag.png",
-           width: 60,
-        ),),
-         Container(
-                 padding: EdgeInsets.only(right:5, top:15, bottom:15),
-       child:  Image.asset(
-          "assets/line.png",
-          width: 30,
-        ),)
-        ]
-
-    ),
-
-      backgroundColor: Colors.white,
-      //backgroundColor: Colors.white,
-
-   body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            right: 0,
-            left: 0,
-            child: Container(
-              height: 120,
-              width: 700,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/img.png"),
-                  fit: BoxFit.fitWidth             )
+    return SafeArea(
+      top: true,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Row(
+            children: [
+              //Padding(padding: EdgeInsets.only(right:50)),
+              Flexible(
+                child: Image.asset(
+                  "assets/teksna.jpeg",
+                  height: 60.0,
+                  width: 100.0,
+                ),
               ),
-          ),),
-
-          Positioned(
-            top: 87,
+            ],
+          ),
+          actions: <Widget>[
+            Container(
+              padding: EdgeInsets.only(right: 8, top: 15, bottom: 15),
+              child: Image.asset(
+                "assets/mall-bag.png",
+                width: 60,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(right: 10, top: 15, bottom: 15),
+              child: Image.asset(
+                "assets/line.png",
+                width: 30,
+              ),
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topCenter,
               child: Container(
-                height: 360,
-                width: MediaQuery.of(context).size.width-40,
-                margin: EdgeInsets.symmetric(horizontal: 20),
+                constraints: BoxConstraints.expand(
+                  height: 240.0,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                decoration: BoxDecoration(color: Colors.white),
+                child: Image.asset("assets/img.png", fit: BoxFit.cover),
+              ),
+            ),
+            Positioned(
+              child: (Container(
+                //height: 360,
+                //width: MediaQuery.of(context).size.width - 40,
+                margin: EdgeInsets.only(top: 100, right: 20, left: 20),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
                   color: Colors.white,
@@ -101,167 +106,175 @@ class _MyHomePageState extends State<MyHomePage> {
                       blurRadius: 10,
                       spreadRadius: 5,
                     )
-                  ]
-                  ),
-
+                  ],
+                ),
                 padding: EdgeInsets.symmetric(horizontal: 15, vertical: 12),
                 child: SingleChildScrollView(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-
                       SizedBox(height: 8),
-
                       Align(
-                        alignment: Alignment(-0.93,0),
-                        child: InkWell(
-                          onTap: () {
-                            //TODO
-                          },
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: "Serif",
-                              // fontStyle: ,
-
+                        alignment: Alignment(-0.93, 0),
+                        child: SafeArea(
+                          top: true,
+                          left: true,
+                          bottom: true,
+                          right: true,
+                          child: InkWell(
+                            onTap: () {
+                              //TODO
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w600,
+                                fontFamily: "Serif",
+                                // fontStyle: ,
+                              ),
                             ),
                           ),
                         ),
                       ),
-
-
                       SizedBox(height: 15),
                       _labelText('Email Address / Phone Number '),
                       _inputTextField('example@example.com', false),
                       SizedBox(height: 15),
                       _labelText('Enter Password'),
                       _inputTextField('********', true),
-
                       SizedBox(height: 15),
                       Align(
                         alignment: Alignment.center,
-                        child: Container(
-                          height: 55,
-                          width: 300,
-                          child: new RaisedButton(
-                            onPressed: () => {
-                              //TODO
-                            },
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                fontSize: 16,
+                        child: SafeArea(
+                          left: true,
+                          right: true,
+                          minimum: const EdgeInsets.all(16.0),
+                          child: Container(
+                            height: 55,
+                            width: 300,
+                            child: new RaisedButton(
+                              onPressed: () => {
+                                //TODO
+                              },
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
                               ),
-                            ),
-                            color: Colors.blue[500],
-                            textColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                              color: Colors.blue[500],
+                              textColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
                             ),
                           ),
                         ),
                       ),
-
-
                     ],
                   ),
                 ),
               )),
-
-          Positioned(
-            top: MediaQuery.of(context).size.height-275,
-            right: 0,
-            left: 0,
-            child: Column(
+            ),
+            Column(
               children: [
-              Divider(
-                color: Colors.blue,
-                height: 5,
-                endIndent: 30,
-                indent: 30,
-
-              ),
-
-              SizedBox(height: 25),
-
-              Text(
-                  "Or sign-in with",
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.grey[500],
-
-              ),),
-
-              SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                   _loginSocialMediaBtn(
-                  'assets/google-icon 1.png'),
-
-                  SizedBox(width: 20),
-
-                  _loginSocialMediaBtn(
-                      'assets/Vector.png'),
-                ],
-              ),
-
-
-            ],),
-          ),
-
-          Positioned(
-            top: MediaQuery.of(context).size.height-130,
-            right: 0,
-            left: 0,
-            child: Column(children: [
-              Text('Don\'t have an account? Sign Up!',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
-                  color: Colors.blue[500],
-                  fontSize: 16,
-                ),)
-            ],),)
-        ],),
+                Flexible(
+                  child: Column(
+                    children: [
+                      Spacer(flex: 24),
+                      Divider(
+                        color: Colors.blue,
+                        height: 5,
+                        endIndent: 30,
+                        indent: 30,
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Or sign-in with",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey[500],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          _loginSocialMediaBtn('assets/google-icon 1.png'),
+                          SizedBox(width: 20),
+                          _loginSocialMediaBtn('assets/Vector.png'),
+                        ],
+                      ),
+                      SizedBox(height: 50),
+                      SafeArea(
+                        bottom: true,
+                        minimum: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Don\'t have an account? Sign Up!',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.blue[500],
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-
 
   //button to login in using scial media,
   _loginSocialMediaBtn(String img) {
     return SizedBox.fromSize(
       size: Size(40, 40),
       child: Container(
-       decoration: new BoxDecoration(
-              border: Border.all(width: 1, color: Colors.blue[400].withOpacity(0.2)),
-              borderRadius: new BorderRadius.all(Radius.circular(5.0)),
-              shape: BoxShape.rectangle,
-            ), //button width and height
-       child:ClipRRect(
-        //borderRadius: BorderRadius.circular(5),
-        child: Material(
-          elevation: 16,
-          shadowColor: Colors.white,
-          color: Colors.white70,
-          child: InkWell(
-            splashColor: Colors.white,
-            onTap: () {},
-            child: Center(
-              child: Image.asset(
-                img,
-                height: 20,
+        decoration: new BoxDecoration(
+          border:
+              Border.all(width: 1, color: Colors.blue[400].withOpacity(0.2)),
+          borderRadius: new BorderRadius.all(Radius.circular(5.0)),
+          shape: BoxShape.rectangle,
+        ), //button width and height
+        child: ClipRRect(
+          //borderRadius: BorderRadius.circular(5),
+          child: Material(
+            elevation: 16,
+            shadowColor: Colors.white,
+            color: Colors.white70,
+            child: InkWell(
+              splashColor: Colors.white,
+              onTap: () {},
+              child: Center(
+                child: Image.asset(
+                  img,
+                  height: 20,
+                ),
               ),
             ),
           ),
         ),
       ),
-    ));
+    );
   }
 
   _inputTextField(hintText, bool obscuretext) {
@@ -274,11 +287,12 @@ class _MyHomePageState extends State<MyHomePage> {
         child: TextField(
           obscureText: obscuretext,
           decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: hintText,
-              hintStyle: TextStyle(
-                color: Colors.black87,
-              )),
+            border: InputBorder.none,
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: Colors.black87,
+            ),
+          ),
         ),
       ),
     );
@@ -298,4 +312,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
